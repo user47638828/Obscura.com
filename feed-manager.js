fa11e297f27b75f830ed15d262e573aa44d82a05
Know me? <he72nkhe5biw7@gmail.com>
2026-03-02 01:33:50 +0100

    Update feed-manager.js

--- feed-manager.js
+++ feed-manager.js
@@ -1,1 +1,15 @@
+import { auth } from "./backend.js"
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js"
 
+const logoutBtn = document.getElementById("logout-btn")
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html"
+  }
+})
+
+logoutBtn.addEventListener("click", async () => {
+  await signOut(auth)
+  window.location.href = "index.html"
+})
